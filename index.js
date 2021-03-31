a3d96d5db7e6fabcec7a39ea584e5d26516ecc66
caitlin <caitlin.wink@gmail.com>
2021-03-31 12:06:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("well at least this works.");
+    const cool = document.querySelector('p');
+    cool.innerHTML.replace("JavaScript is so cool. It lets me add text to my page programmatically.", "This is really cool!")
+});
+
+
+
+
+//cool.addEventListener("replace", function(){
+//function() {
+//     console.log("Well, at least this works");
+//   }); 
+
+// const cool = document.getElementById("text");
+// document.addEventListener("Yeah?", function() {
+//     cool.innerHTML.replace("JavaScript is so cool. It lets me add text to my page programmatically.", "This is really cool!");
+// });
